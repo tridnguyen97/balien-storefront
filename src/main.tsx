5216e79893e326c203a6f954983f65064130af9b
Tri Duc Nguyen <tridnguyen97@gmail.com>
2026-04-24 20:52:36 +0700

    fix the hero section position

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
 import App from './app/App';
+import './styles/index.css';
 import './styles/global.css';
 import store from './lib/store';
 
